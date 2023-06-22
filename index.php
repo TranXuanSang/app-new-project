2383621d87fc75321e172299dabf4923c9d0523e
SangRedComet <sang29631@gmail.com>
2023-06-22 13:57:35 +0700

    update final

--- index.php
+++ index.php
@@ -3,15 +3,15 @@ session_start();
 //$urladmin ="http://localhost:8080/admin/";
 //$urluser ="http://localhost:8080/";
 
-$home = "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/home.php";
-$register = "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/register.php";
-$shoppingcart = "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/shoppingcart.php";
-$order = "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/order.php";
-$logout = "frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/logout.php";
+$home = "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/home.php";
+$register = "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/register.php";
+$shoppingcart = "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/shoppingcart.php";
+$order = "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/order.php";
+$logout = "eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com/logout.php";
 
 //Connection
 
-$conn = mysqli_connect("frwahxxknm9kwy6c.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "eg84xy9f508f9dwf", "jmqam7byju09nogo", "goy9i2r608in7p25")
+$conn = mysqli_connect("eanl4i1omny740jw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "kxvhmvwfindqbr6j", "kxvhmvwfindqbr6j", "dyzpdzy7izih7ans")
       or die("Can not connect database".mysqli_connect_error());
 
 include('theme.php');
